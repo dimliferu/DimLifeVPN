f38f1b314e927e1762ebc04ee52927761cdc5cf0
DimLife <dimkasatin@yandex.ru>
2026-06-11 06:01:23 +0300

    Update constants.dart

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -6,8 +6,8 @@ abstract class Constants {
   static const appName = "Hiddify";
   static const githubUrl = "https://github.com/hiddify/hiddify-next";
   static const licenseUrl = "https://github.com/hiddify/hiddify-next?tab=License-1-ov-file#readme";
-  static const githubReleasesApiUrl = "https://api.github.com/repos/hiddify/hiddify-next/releases";
-  static const githubLatestReleaseUrl = "https://github.com/hiddify/hiddify-app/releases/latest";
+  static const githubReleasesApiUrl = "https://api.github.com/repos/dimliferu/DimLifeVPN/releases";
+  static const githubLatestReleaseUrl = "https://github.com/dimliferu/DimLifeVPN/releases/latest";
   static const appCastUrl = "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
   static const telegramChannelUrl = "https://t.me/hiddify";
   static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
