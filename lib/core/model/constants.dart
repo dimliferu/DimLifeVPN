06cc4b1d94136c143dee4bf5cfc1333c910de9d1
DimLife <dimkasatin@yandex.ru>
2026-06-11 06:45:01 +0300

    Update URLs in constants.dart for DimLifeVPN

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -4,14 +4,14 @@ import 'package:hiddify/utils/utils.dart';
 
 abstract class Constants {
   static const appName = "DimLife";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
+  static const githubUrl = "https://github.com/dimliferu/DimLifeVPN";
   static const licenseUrl = "https://github.com/hiddify/hiddify-next?tab=License-1-ov-file#readme";
   static const githubReleasesApiUrl = "https://api.github.com/repos/dimliferu/DimLifeVPN/releases";
   static const githubLatestReleaseUrl = "https://github.com/dimliferu/DimLifeVPN/releases/latest";
   static const appCastUrl = "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/hiddify";
-  static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
-  static const termsAndConditionsUrl = "https://hiddify.com/terms/";
+  static const telegramChannelUrl = "https://t.me/dimliferu";
+  static const privacyPolicyUrl = "https://github.com/dimliferu/DimLifeVPN";
+  static const termsAndConditionsUrl = "https://github.com/dimliferu/DimLifeVPN";
   static const cfWarpPrivacyPolicy = "https://www.cloudflare.com/application/privacypolicy/";
   static const cfWarpTermsOfService = "https://www.cloudflare.com/application/terms/";
 }
